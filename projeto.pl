4e43691f515bbb242d1f995d15b97549ee127281
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2019-10-25 13:58:03 +0200

    Add files via upload

--- projeto.pl
+++ projeto.pl
@@ -3,7 +3,7 @@ if_else(Q,V,F):- F.
 
 
 :- dynamic produto/3.
-produto(Nome,Desc,Quant).
+%produto(Nome,Desc,Quant).
 
 adicionarProduto(produto(N,D,Q)):- assertz(produto(N,D,Q)).
 adicionarProduto:-
@@ -14,11 +14,31 @@ adicionarProduto:-
     if_else((Quant=<0),((write("Quantidade invalida"),nl,write("Quantidade certa: "),read(Q),assertz(produto(Nome,Desc,Q)))),
              (write("Falso"),assertz(produto(Nome,Desc,Quant)))).
 
-removerProduto(produto(N,D,Q)):- retract(produto(N,D,Q)).
-%removerProduto:- write("Digite o nome do produto: "), read(Nome),retract(produto(Nome,_,_)).
+
+removerProduto:- write("Digite o nome do produto: "), read(Nome),retract(produto(Nome,_,_)).
 
 listaProdutos:- listing(produto).
+
+:- dynamic funcionario/3.
+cadastrarFuncionario:-
+    write("Digite o nome do funcionario: "), read(Nome),
+    write("Digite o usuario para "),write(Usuario),write(": "), read(Usuario),
+    write("Digite a respetiva senha: "), read(Senha),
+    assertz(funcionario(Nome,Usuario,Senha)).
+
+listarFuncionario:- listing(funcionario).
+
+removerFuncionario:-write("Digite o nome do funcionario: "), read(N),retract(funcionario(X,_,_)).
+
+registrarVenda:- nl,
+    write("Nome do produto: "), read(Nome),nl,
+    write("Descricao: "), read(Desc),nl,
+    write("Quantidade vendida: "), read(Qvendida), nl,
+    assertz(produto(Nome,Desc,Qvendida)),produto(Nome,Desc,X), Novaquant is X- Qvendida,nl,
+    write("Atualizado "),write(Novaquant).
+
 menu:-
+    nl,
     write("[1] Adicionar Produto"),nl,
     write("[2] Remover Produto"),nl,
     write("[3] Listar Produto"),nl,
@@ -34,27 +54,25 @@ menu:-
               (if_else((Opc=6),(removerFuncionario),
               (write("Opcao invalida"),menu)))))))))))).
 
-:- dynamic funcionario/3.
-funcionario(Nome,Usuario,Senha).
-cadastrarFuncionario(funcionario(A,B,C)):- assertz(funcionario(A,B,C)).
-cadastrarFuncionario:-
-    write("Digite o nome do funcionario: "), read(Nome),
-    write("Digite o usuario para "),write(Usuario),write(": "), read(Usuario),
-    write("Digite a respetiva senha: "), read(Senha),
-    assertz(funcionario(Nome,Usuario,Senha)).
-
-listarFuncionario:- listing(funcionario).
-
-removerFuncionario(funcionario(N,U,S)) :- retract(funcionario(N,U,S)).
-removerFuncionario:-
-    write("Digite o nome do funcionario: "), read(N),
-    X =funcionario(N,_,_), retract(funcionario(X,_,_)).
+menuUser:- nl,
+            write("[1] Registrar venda"),nl,
+            write("[2] Consultar produto"),nl,
+            write("[3] Listar produto"),nl,
+            read(Opc), if_else((Opc=1),(registrarVenda),(write("Opcao 2 ou 3"))).
 
 login:-
-    write("Digite o user: "), read(User),
-    write("Digite senha: "), read(Senha),
-    if_else(((User=isco),(Senha=1234)),menu,write("Dados errados")).
+    write("Digite o user: (ADMIN) "), read(User),
+    write("Digite senha: (ADMIN) "), read(Senha),
+    if_else(((User=isco),(Senha=1234)),menu,(nl,write("==============================="),nl,
+                                             write("Dados errados, tente novamente"),nl,
+                                             write("================================"),nl,nl,login)).
 
+loginUser:-
+    write("Digite o user: (FUNC) "), read(User),
+    write("Digite a senha: (FUNC) "), read(Senha),
+    if_else(( (User=fam),(Senha=isco)),(menuUser),(nl,write("==================================="),nl,
+                                                   write("Dados errados, tente novamente"),nl,
+                                                   write("==================================="),nl,nl,loginUser)).
 
 
 
